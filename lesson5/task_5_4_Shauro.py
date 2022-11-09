2e283d3821cc9e8474e51ec3c00a5ed9fbe181a5
pudge <beste1997@mail.ru>
2022-11-09 11:20:47 +0300

    сделаны задачи 1,2,3,4,5

---
+++ lesson5/task_5_4_Shauro.py
@@ -0,0 +1,31 @@
+"""
+*4. Создайте программу которая генерирует случайное число в интервале от 1 до 100 и предлагает пользователю угадать это число.
+ В случает ввода от пользователя инструкции «stop», завершает работу и выводит загаданное число
+для генерации случайного числа используйте метод randint модуля random
+"""
+from random import randint
+
+
+def random_game() -> None:
+    """
+    Проходит валидация на попадание пользователем в нужное число
+    """
+    n = randint(0, 10)
+    while True:
+        guess = input("Введите число или команду 'stop': ")
+        if guess == "stop":
+            print(f"Users say 'stop'")
+            break
+        elif n > int(guess):
+            print(f"Your number less")
+        elif guess == "stop":
+            print(f"Users say 'stop'")
+            break
+        elif n < int(guess):
+            print(f"Your number greater")
+        elif n == int(guess):
+            print(f"Your guess number, Congratulations")
+            break
+
+
+random_game()
