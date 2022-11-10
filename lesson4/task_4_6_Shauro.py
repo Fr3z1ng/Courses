07a0cd189d4d5f985cdd9f6605cfcfa424af5858
pudge <beste1997@mail.ru>
2022-11-10 14:29:16 +0300

    обновил задачи 4_6

--- lesson4/task_4_6_Shauro.py
+++ lesson4/task_4_6_Shauro.py
@@ -3,11 +3,9 @@
 в которой первое и последнее слова начинаются с заглавной буквы, а весь остальной текст написан в нижнем регистре.
 """
 s = input("Введите строку:")
-s = s.lower().strip()
-s = s.capitalize()
+s = s.capitalize().strip()
 part_1 = s[:s.rfind(" ") + 1]
 part_2 = s[s.rfind(" ") + 1:]
 part_2 = part_2.capitalize()
 answer = part_1 + part_2
 print(answer)
-
