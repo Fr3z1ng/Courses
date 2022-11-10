05e955201d3bc33c1e48d9a23bab68f4344214f5
pudge <beste1997@mail.ru>
2022-11-10 19:21:23 +0300

    обновил задачи 4_4

--- lesson4/task_4_4_Shauro.py
+++ lesson4/task_4_4_Shauro.py
@@ -15,7 +15,7 @@ set_1 = set(str_1)
 set_2 = set(str_2)
 set_3 = set(str_3)
 
-answer_1 = set_1 & set_2 & set_3
+answer_1 = set_1 | set_2 | set_3
 print("Ответ на первое условие", answer_1)
 answer_2 = (set_1 ^ set_2) | (set_2 ^ set_3)
 print("Ответ на третье условие", answer_2)
