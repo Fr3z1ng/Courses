12c2cacf093bbff88d3e292e467922489818656e
pudge <beste1997@mail.ru>
2022-11-10 14:12:35 +0300

    обновил задачи 4_1 и 5_1(5_2)

--- lesson4/task_4_1_Shauro.py
+++ lesson4/task_4_1_Shauro.py
@@ -8,17 +8,17 @@
 a = int(input("Введите число: "))
 
 
-def sum_number(a: int) -> int:  # решение 1
-    """
-    функция считает сумму чисел при помощи рекурсии
-    """
-    suma = 0
-    if a // 10 == 0:
-        return a
-    else:
-        digit = a % 10
-        suma += digit + sum_number(a // 10)
-    return suma
+# def sum_number(a: int) -> int:  # решение 1
+#     """
+#     функция считает сумму чисел при помощи рекурсии
+#     """
+#     suma = 0
+#     if a // 10 == 0:
+#         return a
+#     else:
+#         digit = a % 10
+#         suma += digit + sum_number(a // 10)
+#     return suma
 
 
 def sum_number_second(a: int) -> int:  # решение 2
@@ -26,8 +26,10 @@ def sum_number_second(a: int) -> int:  # решение 2
     Подсчет каждой цифры в числе через цикл while
     """
     summa = 0
-    while a % 10 != 0:
+    while a != 0:
         digit = a % 10
         a = a // 10
         summa += digit
     return summa
+
+print(sum_number_second(a))
