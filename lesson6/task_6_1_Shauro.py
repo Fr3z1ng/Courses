50e81447c6a914e6c492ff0ea3c322ea2c771132
pudge <beste1997@mail.ru>
2022-11-11 12:48:39 +0300

    сделаны задачи 6_1,6_2,6_3,6_4

---
+++ lesson6/task_6_1_Shauro.py
@@ -0,0 +1,12 @@
+"""
+1. Дан словарь, создать новый словарь, поменяв местами ключ и значение
+"""
+dict_1 = {"лучший": 123, "великолепный": 321}
+# for key, value in dict_1.items(): # первый метод решения
+#     dict_2[value] = key
+# print(dict_2)
+dict_keys = dict_1.keys()
+dict_value = dict_1.values()
+zipped_list = (zip(dict_value, dict_keys)) # объединяем значения в tuple помощью метода zip
+dict_zipped = dict(zipped_list) # преобразуем объект в словарь
+print(dict_zipped)
---
+++ lesson6/task_6_3_Shauro.py
@@ -0,0 +1,25 @@
+"""
+3. Реализуйте функцию get_digits(num: int) -> tuple, которая возвращает кортеж
+цифр заданного целого числа.
+Пример:
+get_digits(87178291199)
+(8, 7, 1, 7, 8, 2, 9, 1, 1, 9, 9)
+"""
+num = int(input("Введите число: "))
+
+
+def get_digits(num: int) -> tuple:
+    """
+    функция возращает кортеж цифр из заданного числа
+    :param num:
+    :return:
+    """
+    s = str(num)
+    new_list = []
+    for i in s:
+        new_list.append(i)
+
+    return tuple(new_list)
+
+if __name__ == '__main__':
+    print(get_digits(num))
