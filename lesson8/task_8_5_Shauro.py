080808b410769eee8d5650eecf3cc02d1a596af1
pudge <beste1997@mail.ru>
2022-11-23 16:21:06 +0300

    обвернул задачу в функцию

--- lesson8/task_8_5_Shauro.py
+++ lesson8/task_8_5_Shauro.py
@@ -6,25 +6,33 @@
 """
 import json
 import csv
+
 json_name = input("Введите имя файла JSON(если же он находится в вашей корневой папке, то просто название: ")
 with open(f"{json_name}.json", mode="r") as file:
     json_string = json.load(file)
-names = []
-ages = []
-id_s = []
-for key, value in json_string.items():
-    id_s.append(key)
-    for name, age in value.items():
-        if name == "name":
-            names.append(age)
-        else:
-            ages.append(age)
 
-one_list = list(zip(names, ages, id_s))
-csv_name = input('Введите название CSV файла в который желаете сохранить данные:')
-with open(f"csv_file.csv", mode="w") as csv_file:
-    fieldnames = ["name", "age", "id"]
-    csv_writer = csv.writer(csv_file, delimiter=",")
-    csv_writer.writerow(["name", "age", "id"])
-    for i in one_list:
-        csv_writer.writerow(list(i))
+
+def transform_to_csv(json_string: str):
+    names = []
+    ages = []
+    id_s = []
+    for key, value in json_string.items():
+        id_s.append(key)
+        for name, age in value.items():
+            if name == "name":
+                names.append(age)
+            else:
+                ages.append(age)
+
+    one_list = list(zip(names, ages, id_s))
+    csv_name = input('Введите название CSV файла в который желаете сохранить данные:')
+    with open(f"{csv_name}.csv", mode="w") as csv_file:
+        fieldnames = ["name", "age", "id"]
+        csv_writer = csv.writer(csv_file, delimiter=",")
+        csv_writer.writerow(["name", "age", "id"])
+        for i in one_list:
+            csv_writer.writerow(list(i))
+
+
+if __name__ == '__main__':
+    transform_to_csv(json_string)
