b25d6af418b604315339f3ccdfd62462344088e0
pudge <beste1997@mail.ru>
2022-12-21 16:20:56 +0300

    добавил вариант из лекции

--- lesson15/task PEEWEE.py
+++ lesson15/task PEEWEE.py
@@ -1,18 +1,46 @@
 from peewee import *
 
-titles = input('Введите альбом: ')
+# titles = input('Введите альбом: ')
+
+# def database_info(titles: str) -> list:
+#     """
+#     Функция подключается к БД и выводит все треки введенного альбома
+#     """
+#     conn = SqliteDatabase('chinook.db') # подключение к БД
+#     cursor = conn.cursor() # для перехода по записям
+#     cursor.execute(f"SELECT Name FROM tracks WHERE AlbumID = (SELECT AlbumID FROM albums WHERE Title = '{titles}')")
+#     result = cursor.fetchall() # заносим в переменную все совпадения
+#     conn.close()
+#     return result
+db = SqliteDatabase('chinook.db')
 
 
-def database_info(titles: str) -> list:
-    """
-    Функция подключается к БД и выводит все треки введенного альбома
-    """
-    conn = SqliteDatabase('chinook.db') # подключение к БД
-    cursor = conn.cursor() # для перехода по записям
-    cursor.execute(f"SELECT Name FROM tracks WHERE AlbumID = (SELECT AlbumID FROM albums WHERE Title = '{titles}')")
-    result = cursor.fetchall() # заносим в переменную все совпадения
-    conn.close()
-    return result
+class BaseModel(Model):
+    class Meta:
+        database = db
 
 
-print(database_info(titles))
+class Albums(BaseModel):
+    title = CharField(column_name='Title')
+    album_id = AutoField(column_name='AlbumID')
+
+    class Meta:
+        table_name = 'albums'
+
+
+class Track(BaseModel):
+    name = CharField(column_name='Name')
+    album_id = AutoField(column_name='AlbumID')
+
+    class Meta:
+        table_name = 'tracks'
+
+
+def get(album_name_from_input):
+    with db as database:
+        tracks = Track.select().where(Track.album_id == Albums.select().where(Albums.title == album_name_from_input))
+        print(tracks)
+    return tracks
+
+
+print([track.name for track in list(get('A-Sides'))])
