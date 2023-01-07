dd8a76c69adb7dc111b645082f9e4a5d929330a7
pudge <beste1997@mail.ru>
2023-01-07 22:38:30 +0300

    обновил домашку по Flask

--- lesson18/flask_app/app/views.py
+++ lesson18/flask_app/app/views.py
@@ -4,24 +4,32 @@ from .validation import Validator, Validation  # добавление задан
 
 app1 = Blueprint('app1', __name__)
 
+url = 'https://api.kanye.rest'
+
 
 @app1.route('/quote')
 def quote():
+    global url
     """
     Вывод цитаты Кани Веста взависимости от введенных данных, /quote&number=3 - выводит 3 цитаты и т.д.
     """
-    quotes = None  # определил переменную для определения есть ли передача цитат или нет
-    for value in dict(request.args).values():
-        quotes = value
+    quote_number = request.args.get('number', 1)  # определил переменную для определения есть ли передача цитат или нет
     some_list = []  # пустой список по которому буду проходить в Jinja что бы показывать определенное кол-во цитат
-    if quotes is None:  # если не приходит ни одна цитата,то по условию вывести одну цитату
-        src = requests.get('https://api.kanye.rest')
+    if quote_number is None:  # если не приходит ни одна цитата,то по условию вывести одну цитату
+        src = requests.get(url)
         for value in src.json().values():
             some_list.append(value)
         return render_template("index.html", some_list=some_list)
-    elif int(quotes) > 0:  # если цитаты все таки есть,то посчитать их кол-во для вывода определенного кол-ва цитат
-        url = 'https://api.kanye.rest'
-        west = [requests.get(url).json()['quote'] for x in range(int(quotes))] # сделал по вашему методу гораздо локаничней
+    elif int(
+            quote_number) > 0:  # если цитаты все таки есть,то посчитать их кол-во для вывода определенного кол-ва цитат
+        try:
+            quote_number = int(quote_number)
+        except ValueError:
+            abort(404)
+        else:
+            west = [requests.get(url).json()['quote'] for x in
+                    range(quote_number)]
+        # сделал по вашему методу гораздо локаничней
 
         # while i <= int(quotes):  # использую цикл while,что бы запрашивать каждый раз новую цитату
         #     src = requests.get('https://api.kanye.rest')
@@ -34,18 +42,12 @@ def quote():
 
 @app1.route('/register', methods=["POST", "GET"])
 def register():
-    username = None
-    password = None
-    email = None
     if request.method == 'POST':  # проверяю является ли данный запрос POST
         src = dict(request.form)  # забираю введенные данные пользователя в форме
-        for key, value in src.items():  # присваиваю переменным их значения для валидации введенных данных
-            if key == 'username':
-                username = value
-            elif key == 'password':
-                password = value
-            elif key == 'email':
-                email = value
+        try:
+            username, password, email = request.form['username'], request.form['password'], request.form['email']
+        except KeyError as e:
+            return f"Поле {e} не было передано"
         validator_1 = Validator(username, password, email)  # создаю объект из 13.3 домашнего задания
         try:
             if validator_1.validation():  # если проходит валидацию до вызываю код 405,что персональные данные отработаны
